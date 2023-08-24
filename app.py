42f01577a256cc497c7b208d6c2f39ff7b29390b
Joshua <jhellst@gmail.com>
2023-08-23 17:15:07 -0700

    Finished functionality of app

--- app.py
+++ app.py
@@ -12,13 +12,20 @@ debug = DebugToolbarExtension(app)
 @app.get('/')
 def generate():
     """Generates and returns input form from prompts"""
+
     return render_template('questions.html', prompts = silly_story.prompts)
+
 
 @app.get('/results')
 def display_story():
     """Takes input from form and displays story"""
-    responses = {}
-    for prompt in silly_story.prompts:
-        responses[prompt] = request.args[prompt]
 
-    return render_template('results.html', responses = responses)
+    # breakpoint() # Pauses code, like debugger
+
+    # responses = {}
+    # for prompt in silly_story.prompts:
+    #     responses[prompt] = request.args[prompt]
+
+    ad_lib_template = silly_story.get_result_text(request.args) # We can pass request.args because it already behaves like a dict
+
+    return render_template('results.html', ad_lib_template=ad_lib_template)
